fff319ab334b5145ae412fae1ec248b7cecc790b
Pepe García <jl.garhdez@gmail.com>
2016-12-09 12:37:30 +0100

    new base config
    completely copied from this fella http://aaronbedra.com/emacs.d

--- init.el
+++ init.el
@@ -1,12 +1,65 @@
-(require 'package)
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(setq user-full-name "Pepe García")
+(setq user-mail-address "jlgarhdez@gmail.com")
 
-(defvar my-packages '(better-defaults paredit idle-highlight-mode ido-ubiquitous
-                                      find-file-in-project magit smex scpaste))
+(setenv "PATH" (concat "/usr/local/bin:/opt/local/bin:/usr/bin:/bin:/home/abedra/.cabal/bin" (getenv "PATH")))
+(setenv "GOPATH" (concat (getenv "HOME") "/src/golang"))
+(add-to-list 'exec-path (concat (getenv "GOPATH") "/bin"))
+(require 'cl)
 
+(load "package")
 (package-initialize)
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-(setq magit-last-seen-setup-instructions "1.4.0")
+(setq package-archive-enable-alist '(("melpa" deft magit)))
+
+(defvar pepegar/packages '(
+                           helm
+                           projectile
+                           helm-projectile
+                           clojure-mode
+                           deft
+                           erlang
+                           flycheck
+                           gist
+                           haskell-mode
+                           magit
+                           markdown-mode
+                           marmalade
+                           org
+                           paredit
+                           restclient
+                           scala-mode
+                           ensime
+                           evil)
+  "Default packages")
+
+(defun pepegar/packages-installed-p ()
+  (loop for pkg in pepegar/packages
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+
+(unless (pepegar/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg pepegar/packages)
+    (when (not (package-installed-p pkg))
+            (package-install pkg))))
+
+(require 'evil)
+(evil-mode 1)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-c p h") 'helm-projectile)
+
+(setq inhibit-splash-screen t
+      initial-scratch-message nil
+      initial-major-mode 'org-mode)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(projectile-global-mode)
+
