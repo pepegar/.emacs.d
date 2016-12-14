2385bcd4572a523b8da6e82ff9e7f4566f1f9f86
Pepe García <jl.garhdez@gmail.com>
2016-12-14 17:53:02 +0100

    delete evil mode, move to use-package

--- lisp/packages.el
+++ lisp/packages.el
@@ -1,51 +1,103 @@
-(require 'cl)
-(load "package")
-(package-initialize)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-(setq package-archive-enable-alist '(("melpa" deft magit)))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
 
-(defvar pepegar/packages '(
-                           helm
-                           projectile
-                           helm-projectile
-                           clojure-mode
-                           deft
-                           erlang
-                           flycheck
-                           gist
-                           haskell-mode
-                           magit
-                           markdown-mode
-                           marmalade
-                           org
-                           paredit
-                           restclient
-                           scala-mode
-                           ensime
-                           evil
-                           evil-leader
-                           evil-magit
-                           monokai-theme
-                           neotree
-                           flycheck
-                           restclient
-                           smart-mode-line
-                           markdown-toc
-                           )
-  "Default packages")
-
-(defun pepegar/packages-installed-p ()
-  (loop for pkg in pepegar/packages
-        when (not (package-installed-p pkg)) do (return nil)
-        finally (return t)))
-
-(unless (pepegar/packages-installed-p)
-  (message "%s" "Refreshing package database...")
+(unless (package-installed-p 'use-package)
   (package-refresh-contents)
-  (dolist (pkg pepegar/packages)
-    (when (not (package-installed-p pkg))
-      (package-install pkg))))
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+(use-package autopair
+  :ensure t)
+
+(use-package helm
+  :ensure t
+  :bind* (
+	  ("C-c p p" . helm-projectile-switch-project)
+	  ("C-c p h" . helm-projectile)
+	  ("M-x" . helm-M-x)
+	  ))
+
+(use-package projectile
+  :ensure t)
+
+(use-package helm-projectile
+  :ensure t)
+
+(use-package clojure-mode
+  :ensure t)
+
+(use-package deft
+  :ensure t)
+
+(use-package flycheck
+  :ensure t)
+
+(use-package gist
+  :ensure t)
+
+(use-package haskell-mode
+  :ensure t)
+
+(use-package hungry-delete
+  :ensure t
+  :defer t
+  :diminish hungry-delete-mode
+  :init
+  (add-hook 'emacs-lisp-mode-hook 'hungry-delete-mode)
+  (add-hook 'haskell-mode-hook 'hungry-delete-mode)
+  (add-hook 'scala-mode-hook 'hungry-delete-mode))
+
+(use-package ns-win
+  :if (eq system-type 'darwin)
+  :config
+  (setq mac-command-modifier 'meta
+        mac-control-modifier 'control
+        mac-right-option-modifier 'control))
+
+(use-package magit
+  :ensure t)
+
+(use-package markdown-mode
+  :ensure t)
+
+(use-package org
+  :ensure t)
+
+(use-package paredit
+  :ensure t)
+
+(use-package restclient
+  :ensure t)
+
+(use-package scala-mode
+  :ensure t)
+
+(use-package ensime
+  :ensure t)
+
+(use-package monokai-theme
+  :ensure t)
+
+(use-package neotree
+  :ensure t)
+
+(use-package flycheck
+  :ensure t)
+
+(use-package restclient
+  :ensure t)
+
+(use-package smart-mode-line
+  :ensure t)
+
+(use-package markdown-toc
+  :ensure t)
 
 (provide 'packages)
--- lisp/setup.el
+++ lisp/setup.el
@@ -1,8 +1,4 @@
 (require 'autopair)
-(require 'evil)
-(evil-mode 1)
-(require 'evil-magit)
-(require 'evil-leader)
 (global-flycheck-mode)
 
 (set-face-attribute 'default nil :family "Source Code Pro" :height 145 :weight 'normal)
@@ -20,41 +16,32 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
-(custom-set-variables
- '(custom-safe-themes
-   (quote
-    ("c5a886cc9044d8e6690a60f33db45506221aa0777a82ad1f7fe11a96d203fa44" default))))
-(custom-set-faces)
-
 (load-theme 'monokai t)
 
 (set-face-attribute 'default nil :height 150)
 
 (global-linum-mode t)
-(global-evil-leader-mode)
 (projectile-mode)
-(add-hook 'neotree-mode-hook
-        (lambda ()
-            (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
-            (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
-            (define-key evil-normal-state-local-map (kbd "q")   'neotree-hide)
-            (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
 
 (setq ensime-startup-notification nil)
+
 (setq ensime-snapshot-notification nil)
+
 (setq sml/no-confirm-load-theme t)
 (sml/setup)
 (setq sml/theme 'dark)
+
 (setq helm-split-window-in-side-p t
-      helm-buffers-fuzzy-matching t
+      helm-mode-fuzzy-match t
+      helm-completion-in-region-fuzzy-match t
       helm-buffer-max-length nil
-      helm-recentf-fuzzy-match t
-      helm-apropos-fuzzy-match t
       helm-move-to-line-cycle-in-source t
       helm-ff-search-library-in-sexp t
       helm-ff-file-name-history-use-recentf t
       helm-ff-auto-update-initial-value t
       helm-full-frame nil
-      helm-split-window-in-side-p t)
+      helm-split-window-in-side-p t
+      helm-M-x-fuzzy-match t
+      )
 
 (provide 'setup)
