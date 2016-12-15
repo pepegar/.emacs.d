50c2af71c5c7a87673a27c726af1844db151b3b2
Pepe García <jl.garhdez@gmail.com>
2016-12-15 09:49:18 +0100

    add multiple cursors

--- lisp/packages.el
+++ lisp/packages.el
@@ -1,3 +1,4 @@
+(require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
@@ -14,7 +15,8 @@
 (require 'bind-key)
 
 (use-package autopair
-  :ensure t)
+  :ensure t
+  :diminish pair-mode)
 
 (use-package helm
   :ensure t
@@ -99,5 +101,37 @@
 
 (use-package markdown-toc
   :ensure t)
+
+
+(use-package multiple-cursors
+  :ensure t
+  :bind (("C-* l" . mc/edit-lines)
+	 ("C-* n" . mc/mark-next-like-this)
+	 ("C-* p" . mc/mark-previous-like-this)
+	 ("C-* C-*" . mc/mark-all-like-this)
+	 ("C-c C-* C-*" . mc/mark-more-like-this)
+
+	 ("C-* i" . mc/insert-numbers)
+	 ("C-* s" . mc/sort-regions)
+	 ("C-* r" . mc/reverse-regions)
+	 ("M-<mouse-1>" . mc/add-cursor-on-click))
+  :init
+  (global-unset-key (kbd "M-<down-mouse-1>"))
+  :config
+  (require 'mc-extras))
+
+(use-package mc-extras
+  :ensure t
+  :commands (mc/compare-chars mc/compare-chars-backward mc/compare-chars-forward
+			      mc/cua-rectangle-to-multiple-cursors
+			      mc/remove-current-cursor mc/remove-duplicated-cursors)
+  :config
+  (progn
+    (bind-keys :map mc/keymap
+	       ("C-. C-d" . mc/remove-current-cursor)
+	       ("C-. d" . mc/remove-duplicated-cursors)
+	       ("C-. =" . mc/compare-chars))
+    (eval-after-load 'cua-base
+      '(bind-key "C-. C-," 'mc/cua-rectangle-to-multiple-cursors cua--rectangle-keymap))))
 
 (provide 'packages)
