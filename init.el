efe92ccd73a9a207bc96bf393d9e040877fe02f3
pepegar <jl.garhdez@gmail.com>
2018-02-19 18:36:24 +0100

    moar

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;;
-;; This file is copied from @danielmai's ~init.el~
+;; This file is adapted from @danielmai's ~init.el~
 ;;
 (setq gc-cons-threshold 400000000)
 
@@ -34,11 +34,23 @@
 ;; From use-package README
 (eval-when-compile
   (require 'use-package))
-(require 'diminish)                ;; if you use :diminish
-(require 'bind-key)
 
 ;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 
 (setq gc-cons-threshold 800000)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (expand-region multiple-cursors flx restclient all-the-icons-dired scala-mode org-bullets hydra counsel ivy flycheck autopair diminish projectile magit-gh-pulls magit use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
