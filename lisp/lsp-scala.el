f95c5cd0853b5f98661a5fb995849b21dbf75035
Pepe García <pepe@pepegar.com>
2019-01-15 21:08:40 +0100

    morrr stufff

--- lisp/lsp-scala.el
+++ lisp/lsp-scala.el
@@ -4,11 +4,11 @@
 
 ;; Author: Ross A. Baker <ross@rossabaker.com>
 ;; Version: 0.0.1
-;; Package-Requires: ((lsp-mode "3.0"))
-;; Keywords: scala, lsp, metals
+;; Package-Requires: ((emacs "24.4") (lsp-mode "5.0") (sbt-mode "2.0"))
+;; Keywords: languages tools scala lsp metals
 ;; URL: https://github.com/rossabaker/lsp-scala
 
-;;; Commentary
+;;; Commentary:
 
 ;; This package defines an lsp-mode client for Scala.
 
@@ -18,15 +18,20 @@
 (require 'sbt-mode)
 
 ;;;###autoload
-(defcustom lsp-scala-server-command '("metals-emacs")
-  "The command to launch the language server,"
+(defcustom lsp-scala-server-command "metals-emacs"
+  "The command to launch the Scala language server."
   :group 'lsp-scala
-  :type 'list)
+  :type 'file)
 
-(defcustom lsp-scala-workspace-root default-directory
-  "The root directory of the workspace,"
+;;;###autoload
+(defcustom lsp-scala-server-args '()
+  "Extra arguments for the Scala language server."
   :group 'lsp-scala
-  :type 'directory)
+  :type '(repeat string))
+
+(defun lsp-scala--server-command ()
+  "Generate the Scala language server startup command."
+  `(,lsp-scala-server-command ,@lsp-scala-server-args))
 
 (defvar lsp-scala--config-options `())
 
@@ -39,30 +44,34 @@
 (defun lsp-scala-build-import ()
   "Unconditionally run `sbt bloopInstall` and re-connect to the build server."
   (interactive)
-  (lsp-send-execute-command "build-import" ())
-  )
+  (lsp-send-execute-command "build-import" ()))
 
 (defun lsp-scala-build-connect ()
   "Unconditionally cancel existing build server connection and re-connect."
   (interactive)
-  (lsp-send-execute-command "build-connect" ())
-  )
+  (lsp-send-execute-command "build-connect" ()))
 
 (defun lsp-scala-doctor-run ()
   "Open the Metals doctor to troubleshoot potential build problems."
   (interactive)
-  (lsp-send-execute-command "doctor-run" ())
-  )
+  (lsp-send-execute-command "doctor-run" ()))
 
 (defun lsp-scala-sources-scan ()
   "Walk all files in the workspace and index where symbols are defined."
   (interactive)
-  (lsp-send-execute-command "source-scan" ())
-  )
+  (lsp-send-execute-command "source-scan" ()))
 
+(with-eval-after-load 'lsp
+  (lsp-register-client
+    (make-lsp-client :new-connection
+           (lsp-stdio-connection 'lsp-scala--server-command)
+		     :major-modes '(scala-mode)
+		     :server-id 'scala)))
+
+;; Legacy support for lsp-mode <= 5
 (lsp-define-stdio-client lsp-scala "scala"
                          (lambda () (sbt:find-root))
-                         lsp-scala-server-command)
+                         (lsp-scala--server-command))
 
 (provide 'lsp-scala)
 ;;; lsp-scala.el ends here
